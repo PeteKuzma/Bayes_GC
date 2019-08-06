d3943117de164caa5ae30dbda41547128af572a5
pkuzma <pete.kuzma@ed.ac.uk>
2019-08-06 15:13:18 +0100

    Pal 5 sh file

---
+++ Script_files/pal5_plumquad.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Grid Engine options (lines prefixed with #$)
+#$ -N Pal5_plumquad
+#$ -cwd
+#$ -l h_rt=48:00:00
+#$ -l h_vmem=4G
+#  These options are:
+#  job name: -N
+#  use the current worcalking directory: -cwd
+#  runtime limit of 5 minutes: -l h_rt
+#  memory limit of 1 Gbyte: -l h_vmem
+#$ -m beas
+#$ -M pkuzma@roe.ac.uk
+. /etc/profile.d/modules.sh
+module load anaconda
+module load openmpi
+source activate PyMN
+export LD_LIBRARY_PATH=$HOME/multinest/MultiNest/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=$HOME/own_codes:$PYTHONPATH
+
+mpirun -n 16 Pal5_plumquad.py
+
+#python -n 16 Pal5_plumquad.py
