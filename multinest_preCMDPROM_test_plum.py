f7b8bbccf48359c8ead188e7f3fad514f70d39e7
pkuzma <pete.kuzma@ed.ac.uk>
2020-04-24 16:42:00 +0100

    Update multinest_isor_cmd_nomoving_plum.py, multinest_preCMDPROM_test_plum.py files

--- multinest_preCMDPROM_test_plum.py
+++ multinest_preCMDPROM_test_plum.py
@@ -56,7 +56,8 @@ class PyMN_RUN(PyNM):
         PyNM.__init__(self,cluster,radius,prior,inner_radii,sample_size,cr,tr,lh,pmra,pmdec,clcut,survey,select=select,pm_sel=pm_sel,live_points=live_points,existing=existing,rmax=rmax,Fadd=Fadd,preking=preking,outbase_add=outbase_add,pmsel=1,phot=phot)
 #PyNM.__init__(self,cluster,radius,prior,inner_radii,sample_size,cr,tr,select=True,pm_sel="norm",live_points=400,existing=False,rmax=4.,Fadd=None,preking=False,outbase_add=None)
         self.King=where(self.dist<=tr,self.L_sat_king(self.x_ps,self.y_ps,self.cr,self.tr),0)
-        self.Plummer=where(self.dist<=tr,self.L_sat_spat_PL(self.x_ps,self.y_ps,self.cr,0,self.rmax),1e-99)
+        #self.Plummer=where(sqrt(self.x_ps*self.x_ps+self.y_ps*self.y_ps)<=tr,self.L_sat_spat_PL(self.x_ps,self.y_ps,self.cr,0,self.rmax),0)
+        self.Plummer=self.L_sat_spat_PL(self.x_ps,self.y_ps,self.cr,0,self.rmax)
         self.Parameters=["x_pm,cl","y_pm,cl","x_dsp,cl","y_dsp,cl","x_pm,MW","y_pm,MW","x_dsp,MW","y_dsp,MW","f_cl","f_ev","theta","k","theta2","k2","gamma","xpm_const","ypm_const"]
         self.N_params = len(self.Parameters)
         self.survey=survey
